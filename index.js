923dd5cc1b9fd4f829a70ad6e6cc159f5f462c4e
Charly <charly2221@hotmail;fr>
2022-01-09 21:51:05 +0100

    apisetup

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/', async(req, res) => {
+    try {
+        res.json({
+            status:200,
+            message:'Hello world'
+        })
+    } catch (error) {
+        console.error(error)
+        return res.status(500).send('Server error')
+    }
+})
+
+module.exports = router;
+
